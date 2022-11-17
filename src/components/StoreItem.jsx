7d8a4cf1c2d1cf48e53d0fc169260d1b84b0419c
gitaymen <steelsahdow@gmail.com>
2022-11-17 13:52:56 +0100

    basic react & bootstrap ecom website

---
+++ src/components/StoreItem.jsx
@@ -0,0 +1,64 @@
+import { Card, Button } from "react-bootstrap";
+import { useShoppingCart } from "../context/ShoppingCartContext";
+import { formatCurrency } from "../utilities/formatCurrency";
+
+function StoreItem({ id, name, price, imgUrl }) {
+  const {
+    getItemQuantity,
+    increaseCartQuantity,
+    decreaseCartQuantity,
+    removeFromCart,
+  } = useShoppingCart();
+
+  const quantity = getItemQuantity(id);
+
+  return (
+    <Card className="h-100">
+      <Card.Img
+        variant="top"
+        src={imgUrl}
+        height="200px"
+        style={{ objectFit: "cover" }}
+      />
+      <Card.Body className="d-flex flex-column">
+        <Card.Title className="d-flex justify-content-between align-items-baseline mb-4">
+          <span className="fs-2">{name}</span>
+          <span className="fs-2 text-muted">{formatCurrency(price)}</span>
+        </Card.Title>
+        <div className="mt-auto">
+          {quantity === 0 ? (
+            <Button className="w-100" onClick={() => increaseCartQuantity(id)}>
+              + Add To Cart
+            </Button>
+          ) : (
+            <div
+              className="d-flex align-items-center flex-column"
+              style={{ gap: ".5rem" }}
+            >
+              <div
+                className="d-flex align-items-center justify-content-center "
+                style={{ gap: ".5rem" }}
+              >
+                <Button onClick={() => increaseCartQuantity(id)}>+</Button>
+                <div>
+                  <span className="fs-3">{quantity}</span> in cart.
+                </div>
+
+                <Button onClick={() => decreaseCartQuantity(id)}>-</Button>
+              </div>
+              <Button
+                variant="danger"
+                size="sm"
+                onClick={() => removeFromCart(id)}
+              >
+                Remove
+              </Button>
+            </div>
+          )}
+        </div>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default StoreItem;
